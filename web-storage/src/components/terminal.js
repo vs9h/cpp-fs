a53a7672db1b21b57e4c325f90972744b450b920
Chizhov Anton <zr9ihi@gmail.com>
2024-06-07 09:47:46 +0300

    Implement communication between web client and the fs

--- web-storage/src/components/terminal.js
+++ web-storage/src/components/terminal.js
@@ -1,30 +1,162 @@
 import { useState, useRef, useEffect } from "react";
+import { config } from "@config/index"
 
 const Terminal = () => {
+    const [session, setSession] = useState({
+        login: "guest",
+        cwd: "/",
+        protocol: config.fs.protocol,
+        host: config.fs.host,
+        port: config.fs.port,
+        verbose: false,
+    });
+
     const [history, setHistory] = useState([]);
     const [input, setInput] = useState('');
     const terminalRef = useRef(null);
     const inputRef = useRef(null);
-    const [login, setLogin] = useState("guest");
-    const [cwd, setCwd] = useState("/");
 
-    const ProcessCmd = (cmd) => {
-        const cmds = cmd.split(' ');
-        const bin = cmds[0]
+    const getFsUrl = () => {
+        return `${session.protocol}://${session.host}:${session.port}`;
+    }
+
+    const setLogin = (login) => {
+        setSession(session => ({ ...session, login }));
+    }
+
+    const setCwd = (cwd) => {
+        setSession(session => ({ ...session, cwd }));
+    }
+
+    const setHost = (host) => {
+        setSession(session => ({ ...session, host }));
+    }
+
+    const setPort = (port) => {
+        setSession(session => ({ ...session, port }));
+    }
+
+    const setVerbose = (verbose) => {
+        setSession(session => ({ ...session, verbose }));
+    }
+
+    const verboseLog = (string) => {
+        if (session.verbose)
+            addToHistory([`*VERBOSE*: ${string}`]);
+    }
+
+    const addToHistory = (newLines) => {
+        setHistory(history => [...history, ...newLines]);
+    }
+
+    const sendQuery = async (route, method = "GET") => {
+        try {
+            const query = `${getFsUrl()}/${route}`;
+            verboseLog(`send query '${query}'`);
+            console.log(`send query '${query}'`);
+            const response = await fetch(query, {
+                method
+            });
+            if (!response.ok)
+                return response.json().then(error => Promise.reject(`${JSON.stringify(error, null, 1)}`));
+
+            return response;
+        } catch (error) {
+            verboseLog(JSON.stringify(error, null, 1));
+            return Promise.reject("internal server error");
+        }
+    }
+
+    const ping = async () => sendQuery("ping").then(response => response.text());
+
+    const ls = async (path) => sendQuery(`ls?path=${path}`).then(response => response.json()).then(object => JSON.stringify(object.entries, null, 1));
+
+    const cat = async (path, offset, size) =>
+        sendQuery(`cat?path=${path}&offset=${offset}&size=${size}`)
+            .then(response => response.text());
+
+    const store = async (dir, file, data) =>
+        sendQuery(`store?dir=${dir}&file=${file}&data=${data}`, "POST")
+            .then(response => response.json()).then(object => JSON.stringify(object, null, 1));
+
+    const mkdir = async (at, dir) =>
+        sendQuery(`mkdir?at=${at}&dir=${dir}`, "POST")
+            .then(response => response.json()).then(object => JSON.stringify(object, null, 1));
+
+    const squashConsecutiveSlashes = (str) =>  str.replace(/\/+/g, '/');
+
+    const ProcessCmd = async (cmd) => {
+        const [bin, ...cmds] = cmd.split(' ');
         if (bin == "login") {
-            setLogin(cmds[1]);
+            setLogin(cmds[0]);
             return null;
         }
         if (bin == "cwd") {
-            setCwd(cmds[1]);
+            setCwd(cmds[0]);
             return null;
-        }
-        if (bin == "echo") {
-            return cmd.substr(bin.length + 1);
         }
         if (bin == "cd") {
-            setCwd(cmds[1][0] == '/' ? cmds[1] : `${cwd}/${cmds[1]}`)
+            const path = cmds[0][0] == '/' ? cmds[0] : squashConsecutiveSlashes(`${session.cwd}/${cmds[0]}`);
+            return ls(path).then((_) => { setCwd(path); return null; })
+        }
+        if (bin == "ping") {
+            return ping();
+        }
+        if (bin == "verbose") {
+            const verbose = !session.verbose;
+            setVerbose(verbose);
+            return `set verbose to '${verbose}'`;
+        }
+        if (bin == "host") {
+            setHost(cmds[0]);
             return null;
+        }
+        if (bin == "port") {
+            setPort(cmds[0]);
+            return null;
+        }
+        if (bin == "info") {
+            return `Session info: ${JSON.stringify(session, null, 1)}`;
+        }
+        if (bin == "ls") {
+            let path = session.cwd;
+            if (cmds.length !== 0) {
+                if (cmds[0][0] == '/')
+                    path = cmds[0];
+                else
+                    path = `${path}/${cmds[0]}`
+            }
+
+            return ls(path);
+        }
+        if (bin === "cat") {
+            if (cmds.length == 0)
+                return "Please, specify path to cat";
+
+            let path = session.cwd;
+            if (cmds[0][0] == '/')
+                path = cmds[0];
+            else
+                path = `${path}/${cmds[0]}`
+
+            const offset = cmds.length > 1 ? cmds[1] : "";
+            const size = cmds.length > 2 ? cmds[2] : "";
+
+
+            return cat(path, offset, size);
+        }
+        if (bin === "store") {
+            if (cmds.length < 2)
+                return "Please, specify file name and data to store";
+
+            const [file, ...data] = cmds;
+            return store(session.cwd, file, data.join(' '));
+        }
+        if (bin === "mkdir") {
+            if (cmds.length !== 1)
+                return "Please, specify new directory name";
+
+            return mkdir(session.cwd, cmds[0]);
         }
 
         return 'Command not found';
@@ -63,19 +195,21 @@ const Terminal = () => {
 
     const getInputPrefix = () => {
         const toSpan = (text, color) => <span style={{ color: color, textWrap: 'nowrap' }}>{text}</span>;
-        return <>{toSpan(login, '#FF79C6')}:{toSpan(cwd, "#50FA7B")}<span style={{ whiteSpace: 'pre' }}> $ </span></>;
+        return <>{toSpan(session.login, '#FF79C6')}:{toSpan(session.cwd, "#50FA7B")}<span style={{ whiteSpace: 'pre' }}> $ </span></>;
     }
-
 
     const processCommand = (cmd) => {
         if (cmd == "clear") {
             setHistory([])
             return;
         }
-        const cmdOut = ProcessCmd(cmd);
-        const newLines = cmdOut === null ? [] : [cmdOut];
-        console.log("sethistory", history);
-        setHistory([...history, <>{getInputPrefix()}{cmd}</>, ...newLines]);
+
+        const addCmdOutput = (cmdOut) => {
+            addToHistory(cmdOut === null ? [] : [cmdOut]);
+        }
+
+        addToHistory([<>{getInputPrefix()}{cmd}</>]);
+        ProcessCmd(cmd).then(addCmdOutput, addCmdOutput);
     };
 
     return (
@@ -105,6 +239,7 @@ const Terminal = () => {
                     ref={inputRef}
                     type="text"
                     value={input}
+                    spellCheck="false"
                     onChange={handleInputChange}
                     onKeyDown={handleKeyDown}
                     style={{
---
+++ web-storage/src/config/index.js
@@ -0,0 +1,8 @@
+
+export const config = {
+    fs: {
+        protocol: process.env.NEXT_PUBLIC_PROTOCOL,
+        host: process.env.NEXT_PUBLIC_FS_HOST,
+        port: process.env.NEXT_PUBLIC_FS_PORT,
+    },
+};
