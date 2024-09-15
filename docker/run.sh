83fbb6aee62596c942c9474c10dc27f9f5b04fdc
Michael Polyntsov <Arno9148@gmail.com>
2024-09-16 02:52:19 +0300

    Explicitly specify /bin/bash as an entrypoint for docker container

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 
 CONTAINER_NAME="${USER}_cppfs"
 
-docker run -it --name "$CONTAINER_NAME" -v "$(pwd)/../:/cppfs" -p 4443:4443 cppfs
+docker run -it --entrypoint /bin/bash --name "$CONTAINER_NAME" -v "$(pwd)/../:/cppfs" -p 4443:4443 cppfs
