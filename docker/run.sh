59985368204fd2e7d9fe3936d13a8be5495d6b5d
Michael Polyntsov <Arno9148@gmail.com>
2024-09-19 17:03:43 +0300

    Expose ports 3000 and 8080 in docker

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,9 @@
 
 CONTAINER_NAME="${USER}_cppfs"
 
-docker run -it --entrypoint /bin/bash --name "$CONTAINER_NAME" -v "$(pwd)/../:/cppfs" -p 4443:4443 cppfs
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+
+docker run -it --entrypoint /bin/bash --name "$CONTAINER_NAME" -v "$(pwd)/../:/cppfs"\
+    -p 4443:4443 -p 3000:3000 -p 8080:8080 cppfs
